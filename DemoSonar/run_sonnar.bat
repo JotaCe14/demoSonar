5088398af5b3caf01b059257d746a87ebb7b4e51
JotaCe14 <jose.bustamante@dbtarget.pe>
2024-11-12 15:26:48 -0500

    bash updated

--- DemoSonar/run_sonnar.bat
+++ DemoSonar/run_sonnar.bat
@@ -1,3 +1,3 @@
-dotnet sonarscanner begin /o:"usuariotmp001" /k:"usuariotmp001_demoSonar" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716" /d:sonar.scanner.scanAll=false
+dotnet sonarscanner begin /o:"jotace14" /k:"JotaCe14_demoSonar" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.token="9f33e2eb1b4970e538f6443adaadb1a82aed40da" /d:sonar.scanner.scanAll=false
 dotnet build
-dotnet sonarscanner end /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716"
+dotnet sonarscanner end /d:sonar.token="9f33e2eb1b4970e538f6443adaadb1a82aed40da"
